fdf9b239de2284f85a4c6cddd3ddca3995c3a2f6
Jan Lelis <mail@janlelis.de>
2013-08-22 17:32:51 +0200

    add code and readme

--- lib/local_port.rb
+++ lib/local_port.rb
@@ -1,1 +1,25 @@
-require 'local_port/version'
+require 'socket'
+require 'timeout'
+
+module LocalPort
+  VERSION = '0.1.0'
+
+  def self.free?(port, seconds=1)
+    Timeout::timeout(seconds) do
+      begin
+        TCPSocket.new("127.0.0.1", port).close
+        true
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+        false
+      end
+    end
+  rescue Timeout::Error
+    false
+  end
+
+  def self.next_free_one(port)
+    port += 1 while free?(port)
+    port
+  end
+end
+
