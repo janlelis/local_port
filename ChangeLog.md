1f9bfb967ac4b21f98d7c9fcbf80f197bc209fe3
Jan Lelis <hi@ruby.consulting>
2021-10-21 22:03:43 +0200

    Fix and improve implementation

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,6 +2,7 @@
 
 ## 1.0.0 / 2021-10-21
 
+- Fix LocalPort#free? - Actually return true for free ports
 - Use socket's timeout instead of timeout stdlib
 
 ## 0.1.0 / 2013-08-22
--- lib/local_port.rb
+++ lib/local_port.rb
@@ -5,20 +5,14 @@ module LocalPort
   VERSION = '0.1.0'
 
   def self.free?(port, seconds=1)
-    Timeout::timeout(seconds) do
-      begin
-        TCPSocket.new("127.0.0.1", port).close
-        true
-      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-        false
-      end
-    end
-  rescue Timeout::Error
+    Socket.tcp("127.0.0.1", port, connect_timeout: 1)
     false
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+    true
   end
 
   def self.next_free_one(port)
-    port += 1 while free?(port)
+    port += 1 until free?(port)
     port
   end
 end
